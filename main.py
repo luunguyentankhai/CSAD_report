702df5262edac9a9aad8a4bc35096b654e1cbe95
hnkk <100778377+luunguyentankhai@users.noreply.github.com>
2026-04-18 09:10:08 +0700

    rebuild main.py

--- main.py
+++ main.py
@@ -1,70 +1,27 @@
-from pathlib import Path
-from scripts import Collectdata, Processed, CSAD
-import pandas as pd
-import numpy as np
+from scripts import Collectdata, Processed
+from scripts.Models import CSAD, OLS
 from time import perf_counter
 
-BASE_URL = Path(__file__).resolve().parent
-
-DATA_URL = BASE_URL / "data"
-
-
 class Main:
-    def Harvester(self):
-        return Collectdata.Data_Harvester().Harvester()
+    def __init__(self):
+        self.CD = Collectdata.Data_Harvester()
+        self.PD = Processed.Processed()
+        
+        #models
+        self.CSAD = CSAD.CSAD()
+        self.OLS = OLS
+def main():
+    start = Main()
+    start.CD.Harvester()
+    start.PD.GetDate()
+    # csad_t = start.CSAD.Covid_dummy_variabel()
+    # start.OLS.OLS_Analyzer(csad_t).interaction_OLS()
+    # start.OLS.OLS_Analyzer(csad_t).sub_sample_OLS()
+    
 
-    def GetDate(self):
-        return Processed.Processed().GetDate()
-
-    def Covid(self):
-        start_time = perf_counter()
-
-        self.Harvester()
-        self.GetDate()
-
-        start_pre_covid = "01/01/2019"
-        end_pre_covid = "31/01/2020"
-
-        start_in_covid = "01/02/2020"
-        end_in_covid = "31/03/2022"
-
-        start_post_covid = "01/04/2022"
-        end_post_covid = "01/01/2024"
-
-        processer = CSAD.CSAD()
-        start_date = "01/01/2019"
-        end_date = "01/01/2024"
-
-        rit = processer.R_i_t(start_date, end_date)
-
-        if not rit.empty:
-            rmt = processer.R_m_t(rit)
-            final_df = processer.Final_Regression_Data(rmt)
-
-            H1 = (
-                final_df["Ngay"] >= pd.to_datetime(start_pre_covid, format="%d/%m/%Y")
-            ) & (final_df["Ngay"] <= pd.to_datetime(end_pre_covid, format="%d/%m/%Y"))
-            H2 = (
-                final_df["Ngay"] >= pd.to_datetime(start_in_covid, format="%d/%m/%Y")
-            ) & (final_df["Ngay"] <= pd.to_datetime(end_in_covid, format="%d/%m/%Y"))
-            H3 = (
-                final_df["Ngay"] >= pd.to_datetime(start_post_covid, format="%d/%m/%Y")
-            ) & (final_df["Ngay"] <= pd.to_datetime(end_post_covid, format="%d/%m/%Y"))
-
-            final_df["H1"] = np.where(H1, 1, 0)
-            final_df["H2"] = np.where(H2, 1, 0)
-            final_df["H3"] = np.where(H3, 1, 0)
-
-            final_df.to_csv(
-                DATA_URL / "processed" / "Final.csv", index=False, encoding="utf-8-sig"
-            )
-
-            print(final_df)
-
-        end_time = perf_counter()
-        total_time = end_time - start_time
-        print(f"Tổng thời gian chạy : {total_time:.4f} giây")
-
-
-start = Main()
-start.Covid()
+if __name__ == "__main__":
+    start_time = perf_counter()
+    main()
+    end_time = perf_counter()
+    total_time = end_time - start_time
+    print(f"Tổng thời gian chạy : {total_time:.4f} giây")
